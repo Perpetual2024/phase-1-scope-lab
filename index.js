961f5d13e45dfa40a32b1b02ec7ecb35fb654137
Perpetual Akinyi <you@example.com>
2024-09-28 22:58:16 +0300

    updates

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+// Write your solution in this file
+window.customerName = 'bob'
+function upperCaseCustomerName(){
+    window.customerName = window.customerName.toUpperCase();
+}
+function setBestCustomer(){
+    window.bestCustomer= "not bob";
+}
+function overwriteBestCustomer(newBestcustomer){
+window.bestCustomer = newBestcustomer;}
+const LeastFavoriteCustomer = "someone else";
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = 'someone new';
+}
+   
+
+
+
